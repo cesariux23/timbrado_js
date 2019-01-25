da2622a3373ea7750cb9c50cc1fbfd981c2abbf6
Cesar Encarnación <cesariux23@gmail.com>
2019-01-24 19:26:50 -0600

    Se lee el archivo :)

--- src/Timbrado/Timbrado.js
+++ src/Timbrado/Timbrado.js
@@ -1,5 +1,24 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Input, Label, Row, Col } from 'reactstrap';
+import { Form, FormGroup, Input, Label, Row, Col, Alert } from 'reactstrap';
+import XLSX from 'xlsx';
+
+// components
+const ShowDetails = function (props) {
+  // return render () => {
+  //   if (props.show) {
+    if (props.message) {
+      return <Alert color={props.color || "warning"}>{props.message}</Alert>
+    } else {
+      return <Alert color="primary">Seleccione un archivo</Alert>
+    }
+  //     else {
+  //       return <Alert color="primary"> seleccione un archivo</Alert>;
+  //     }
+  //   }
+  // }
+}
+
+//main class
 class Timbrado extends Component {
 
   constructor(props) {
@@ -10,11 +29,19 @@ class Timbrado extends Component {
       ejercicio: 2019,
       quincena: '01',
       periodo: 0,
-      descripcion: ''
+      descripcion: '',
+      fechaInicio: '',
+      fechaFin: '',
+      fechaPago: '',
+      plantilla: false,
+      color: "primary",
+      message: '',
+      bae: {}
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleFiles = this.handleFiles.bind(this);
     this.calculaPeriodo = this.calculaPeriodo.bind(this);
+    this.loadFile = this.loadFile.bind(this);
     //this.handleSubmit = this.handleSubmit.bind(this);
 
     // Patrón para validar el nombre del archivo
@@ -33,6 +60,23 @@ class Timbrado extends Component {
       'NOV': 'NOVIEMBRE',
       'DIC': 'DICIEMBRE'
     };
+    this.dataFields = [
+      'RFC',
+      'CURP',
+      'ADSCRIPCION',
+      'NOMBRE',
+      'TPERCEP',
+      'TDEDUC',
+      'TNETO',
+      // campos que talvez no sean necesarios
+      // 'CODIGO',
+      // 'NSS',
+      // 'NCUENTA',
+      // 'IDPAGO',
+      // 'NOEMPEADO',
+      // 'NOMBRE_PUESTO',
+      // 'CORREO',
+    ];
   }
 
   handleChange(event) {
@@ -47,19 +91,36 @@ class Timbrado extends Component {
 
   calculaPeriodo(tipo, valores) {
     var descripcion = '';
+    var fi = '';
+    var ff = '';
+    var anio = parseInt(valores.ejercicio)
+    var arrayMeses = Object.keys(this.meses);
     switch (tipo) {
       case 'm':
-        descripcion = 'PAGO DEL MES DE ' + this.meses[valores.quincena] + ' DE ' + valores.ejercicio;
+        descripcion = 'PAGO DEL MES DE ' + this.meses[valores.quincena] + ' DE ' + valores.ejercicio + ' ' + valores.prefijo;
+        // se calculan las fechas
+        fi = new Date(anio, arrayMeses.indexOf(valores.quincena), 1);
+        ff = new Date(anio, arrayMeses.indexOf(valores.quincena)+1, 0);
         break;
       case 'q':
         var segunda = valores.quincena % 2 === 0;
-        var mes = (segunda ? parseInt(valores.quincena) : parseInt(valores.quincena) + 1) / 2;
-        var nombre_mes = Object.keys(this.meses)[mes-1];
-        descripcion = 'PAGO DE LA ' + (segunda ?'SEGUNDA' : 'PRIMERA') + ' QUINCENA DEL MES DE '+ this.meses[nombre_mes] + ' DE ' + valores.ejercicio;
+        var mes = ((segunda ? parseInt(valores.quincena) : parseInt(valores.quincena) + 1) / 2) - 1;
+        // se calculan las fechas
+        fi = new Date(anio, mes, 1);
+        ff = new Date(anio, mes, 15);
+        if (segunda) {
+          fi = new Date(anio, mes, 16);
+          ff = new Date(anio, mes + 1, 0);
+        }
+        var nombre_mes = arrayMeses[mes];
+        descripcion = 'PAGO DE LA ' + (segunda ?'SEGUNDA' : 'PRIMERA') + ' QUINCENA DEL MES DE '+ this.meses[nombre_mes] + ' DE ' + valores.ejercicio + ' ' + valores.prefijo;
         break;
       default:
        descripcion = '';
     }
+    valores.fechaInicio = fi.toISOString().substr(0, 10);
+    valores.fechaFin = ff.toISOString().substr(0, 10);
+    valores.fechaPago= ff.toISOString().substr(0, 10);
     return descripcion;
   }
 
@@ -73,7 +134,13 @@ class Timbrado extends Component {
       tipo_nomina: 1,
       periodo: 0,
       envio: 0,
-      descripcion: ''
+      descripcion: '',
+      fechaInicio: '',
+      fechaFin: '',
+      fechaPago: '',
+      plantilla: false,
+      message: '',
+      bae: {}
     };
     if (coincidencias) {
       if (coincidencias[1]) {
@@ -84,23 +151,27 @@ class Timbrado extends Component {
       }
       // tipo de nomina
       if (coincidencias[4]) {
-        switch (coincidencias[4].toUpperCase()) {
+        valores.prefijo = coincidencias[4].toUpperCase();
+        switch (valores.prefijo) {
           case 'BASE':
           case 'B':
             valores.tipo_nomina = 1;
+            valores.plantilla = true;
             break;
-            case 'CONF':
-            case 'C':
-              valores.tipo_nomina = 2;
-              break;
-            case 'COMPEN':
-            case 'EDD':
-              valores.tipo_nomina = 3;
-              break;
-              case 'HON':
-              case 'H':
-                valores.tipo_nomina = 4;
-                break;
+          case 'CONF':
+          case 'C':
+            valores.tipo_nomina = 2;
+            valores.plantilla = true;
+            break;
+          case 'COMPEN':
+          case 'EDD':
+            valores.tipo_nomina = 3;
+            valores.plantilla = true;
+            break;
+          case 'HON':
+          case 'H':
+            valores.tipo_nomina = 4;
+            break;
           default:
             valores.tipo_nomina = 5;
         }
@@ -112,12 +183,12 @@ class Timbrado extends Component {
           case 'EXTRA':
           case 'AGUI':
           case 'RETRO':
-            valores.emision = 2;
+            valores.periodo = 2;
             break;
           default:
             //mensual
             valores.quincena = coincidencias[3].toUpperCase()
-            valores.emision = 1;
+            valores.periodo = 1;
             valores.descripcion = this.calculaPeriodo('m', valores);
         }
       }  else {
@@ -125,10 +196,41 @@ class Timbrado extends Component {
         // se debe determinar la quincena en la cual se esta aplicando el pago
         valores.descripcion = this.calculaPeriodo('q', valores);
       }
+
+      this.setState(valores);
+
+      // los datos iniciales del timbrado estan completos
+      // ahora hay que validar el contenido del Archivo
+
+      // lectura del Archivo
+      const reader = new FileReader();
+      reader.addEventListener("load", this.loadFile);
+      reader.readAsBinaryString(file);
+
     } else {
-      alert('el nombre de archivo no coincide con el formato establecido');
+      this.setState({ message: 'El nombre de archivo no coincide con el formato establecido', color: "danger"});
     }
-    this.setState(valores);
+  }
+
+  loadFile (event) {
+    var data = event.target.result;
+    var color = "danger";
+    let wb = XLSX.read(data, {type: 'binary'});
+    let names = wb.SheetNames;
+    let base = XLSX.utils.sheet_to_json(wb.Sheets[names[0]]);
+    let encabezados = Object.keys(base[0]);
+    console.log(encabezados);
+    const faltantes = [];
+    this.dataFields.forEach(item => {
+      if(encabezados.indexOf(item) < 0) {
+        faltantes.push(item);
+        console.log('esta: '+ item);
+      }
+    })
+    if (faltantes.length) {
+      this.setState({message: `No existen en el archivo las columnas: ${faltantes.join(', ')}`, color});
+    }
+    console.log(base);
   }
 
   render () {
@@ -197,22 +299,23 @@ class Timbrado extends Component {
           <Col>
             <FormGroup>
               <Label for="fechaInicio">Fecha de inicio</Label>
-              <Input id="fechaInicio" name="fechaInicio" type="date"/>
+              <Input id="fechaInicio" name="fechaInicio" type="date" value={this.state.fechaInicio} onChange={this.handleChange}/>
             </FormGroup>
           </Col>
           <Col>
             <FormGroup>
               <Label for="fechaFin">Fecha fin</Label>
-              <Input id="fechaFin" name="fechaFin" type="date"/>
+              <Input id="fechaFin" name="fechaFin" type="date" value={this.state.fechaFin} onChange={this.handleChange}/>
             </FormGroup>
           </Col>
           <Col>
             <FormGroup>
               <Label for="fechaPago">Fecha pago</Label>
-              <Input id="fechaPago" name="fechaPago" type="date"/>
+              <Input id="fechaPago" name="fechaPago" type="date" value={this.state.fechaPago} onChange={this.handleChange}/>
             </FormGroup>
           </Col>
         </Row>
+        <ShowDetails message={this.state.message} color={this.state.color}></ShowDetails>
       </Form>
     );
   }
